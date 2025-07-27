367133a22ec7129987433dc8f2e0abbda1e47378
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-07-27 20:48:01 -0300

    Adding the "download area" section to the application

---
+++ src/components/DownloadArea/index.jsx
@@ -0,0 +1,11 @@
+import './DownloadArea.css';
+
+const DownloadArea = (props) => {
+    return (
+        <div>
+            <h2>{props.tituloAba}</h2>
+            <a href="https://www.linkedin.com/in/joao-longuinho/">Linkedin</a>
+        </div>
+    )
+}
+export default DownloadArea
